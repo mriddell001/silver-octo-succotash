620b904284989ebaf8b310e679393949242f5143
Matthew Riddell <matthewriddell@Matthews-iMac.att.net>
2017-04-12 06:57:42 -0700

    Update for loop for forward elimination.

--- serial_gaussian_elimination.c
+++ serial_gaussian_elimination.c
@@ -11,17 +11,29 @@ Description - This implements a serial version of Gaussian elimination with
 #include <stdlib.h>
 
 int main(int argc, char const *argv[]) {
+  char ch;
   int n = atoi(argv[1]);
   int m = n + 1;
   double *matrix = malloc (sizeof (double) * m * n);
   if (n < 5) {
-    /* code */
+    for (size_t i = 0; i < m*n; i++) {
+      double tmp;
+      scanf("%lf", &tmp);
+      ch = getchar();
+      *(matrix + i) = tmp;
+    }
   }
   else {
     time_t t;
     srand48(t);
     for (int i = 0; i < m*n; i++) {
-      *(matrix + i) = (drand48() - 0.5)*1000000;
+      *(matrix + i) = ((drand48() - 0.5)/0.5)*1000000;
+    }
+  }
+  //Forward elimination
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+      /* code */
     }
   }
   return 0;
